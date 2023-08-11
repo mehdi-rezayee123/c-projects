b276ea07fe0a8e449d47b29009c2e744c195456e
mehdi-rezayee123 <140790841+mehdi-rezayee123@users.noreply.github.com>
2023-08-11 17:48:53 +0930

    Add files via upload

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-void print_matrix( int array[10][10], int n)
+void print_matrix( int array[][10], int n)
 {
     for (int i=0; i<n; i++){
         for ( int j = 0;j<n; j++){
